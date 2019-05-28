90477ca0b1fd395d2e0a15dbc010506f47515db8
yongbingao <ben.gao25@gmail.com>
2019-05-28 17:07:39 -0400

    Prepare for initial Heroku push

--- frontend/greens.jsx
+++ frontend/greens.jsx
@@ -8,10 +8,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root');
 
     const store = configureStore();
-    ReactDOM.render(<Root store={store} />, root);
+    ReactDOM.render(<h1>Greens</h1>, root);
+    // ReactDOM.render(<Root store={store} />, root);
 })
 
 //TESTING ONLY
-window.signupUser = signupUser;
-window.loginUser = loginUser;
-window.logout = logout;
+// window.signupUser = signupUser;
+// window.loginUser = loginUser;
+// window.logout = logout;
