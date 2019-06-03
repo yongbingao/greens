bf5e2cd2e32a0d621c8c3876d5fc07ffacc58978
yongbingao <ben.gao25@gmail.com>
2019-06-03 17:29:42 -0400

    Remove iex API key

--- frontend/util/prices_api_util.js
+++ frontend/util/prices_api_util.js
@@ -2,13 +2,13 @@ export const fetchPrices = (ticker, timeframe) => {
     // debugger
     return $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/stable/stock/${ticker}/chart/${timeframe}?token=pk_40288b9cae1546e59ac36ce3176f705c`
+        url: `https://cloud.iexapis.com/stable/stock/${ticker}/chart/${timeframe}?token=${window.iexAPIKey}`
     })
 }
 
 export const fetchNews = ticker => {
     return $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/stable/stock/${ticker}/news/last/5?token=pk_40288b9cae1546e59ac36ce3176f705c`
+        url: `https://cloud.iexapis.com/stable/stock/${ticker}/news/last/5?token=${window.iexAPIKey}`
     })
 }
